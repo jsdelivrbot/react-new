0eedced5fe1fea4fbceacfc41bd9c6d1adca483e
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2017-04-12 11:05:18 +0530

    Fetched YouTube API data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBvcOQaL06BaTDLLh1wwj95196DRZtL1pk';
+
+/*
+* Fetching The API Data EndPoints
+*/
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
+	console.log(data);
+})
 
 //<SearchBar /> => Creating Instance of SearcBar
 
